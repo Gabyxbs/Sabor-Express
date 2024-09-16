4e83b28008d3776baff597b4d164a264cc9eaf15
Gabrielly Freire <gabriellyfre169@gmail.com>
2024-09-16 15:13:17 -0300

    Resposta da escolha

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 print('𝓢𝓪𝓫𝓸𝓻 𝓮𝔁𝓹𝓻𝓮𝓼𝓼\n')
 
 print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
+print('2. Listar restaurantes')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção: {opcao_escolhida}')
+opcao_escolhida = int(input('Escolha uma opção: '))
+
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print('Ativar restaurante')
+else:
+    print('Encerrando o programa')
