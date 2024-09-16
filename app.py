10fd4ebf369ed0849aa7232e955ef4232021c494
Gabrielly Freire <gabriellyfre169@gmail.com>
2024-09-16 14:54:06 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+print('𝓢𝓪𝓫𝓸𝓻 𝓮𝔁𝓹𝓻𝓮𝓼𝓼\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção: {opcao_escolhida}')
