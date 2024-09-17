b9f3d64fdd309fb16042e6c92fba6c2f6c718536
Gabrielly Freire <gabriellyfre169@gmail.com>
2024-09-17 14:01:48 -0300

    função for

--- app.py
+++ app.py
@@ -29,6 +29,15 @@ def cadastrar_novo_restaurante():
     input('Digite uma tecla para voltar no menu pricipal: ')
     main()
 
+def listar_restaurantes():
+    os.system('cls')
+    print('Listando os restaurantes\n')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+    input('\nDigite uma tecla para voltar no menu pricipal: ')
+    main()
+
 def escolher_opcao():
      try:
         opcao_escolhida = int(input('Escolha uma opção: '))
@@ -36,7 +45,7 @@ def escolher_opcao():
         if opcao_escolhida == 1:
             cadastrar_novo_restaurante()
         elif opcao_escolhida == 2:
-            print('Listar restaurantes')
+            listar_restaurantes()
         elif opcao_escolhida == 3:
             print('Ativar restaurante')
         elif opcao_escolhida == 4:
